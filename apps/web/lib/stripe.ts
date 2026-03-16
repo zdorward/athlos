cc56b3b0df3fc7fe0c18b7ca1a7ee1781faaeff3
Zack Dorward <zdorward1@gmail.com>
2026-03-15 21:26:16 -0600

    feat: add stripe client singleton

---
+++ apps/web/lib/stripe.ts
@@ -0,0 +1,9 @@
+import Stripe from "stripe"
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not set")
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2026-02-25.clover",
+})
